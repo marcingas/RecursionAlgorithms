2707f0dc63bc19f5a88ea453838911bc1ef58368
Marcin <gasiorek.mail@gmail.com>
2023-07-14 12:21:53 +0200

    SetMAtrixZero extracted code to methods made it more readable

--- src/simplearrayalgorithms/SetMatrixZero.java
+++ src/simplearrayalgorithms/SetMatrixZero.java
@@ -9,56 +9,84 @@ public class SetMatrixZero {
         int[][] arr = new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
         int n = arr.length;
         int m = arr[0].length;
-//        for(var el : setMatrixToZerosBRute(arr,m,n))
-//        for (var el : setMatrixToZerosBetter(n, m, arr))
-        for (var el : setMatrixToZerosOptimal(arr,n,m))
-            System.out.println(Arrays.toString(el));
+
+//        bruteSolution(arr, n, m);
+//
+//        betterSolution(arr, n, m);
+
+        optimalSolution(arr, n, m);
+
+
     }
 
-    public static void markRows(int m, int[][] arr, int i) {
-        for (int j = 0; j < m; j++) {
-            if (arr[i][j] != 0) {
-                arr[i][j] = -1;
-            }
-        }
+    private static void optimalSolution(int[][] arr, int n, int m) {
+        int colZero = 1;
+        optimalFillArrWithZero(arr, n, m, colZero);
+        optimalFillFromOneWithZero(arr, n, m);
+        optimalFillFirstRowColWithZero(arr, n, m, colZero);
+        printAns(arr);
     }
 
-    public static void markColums(int n, int[][] arr, int j) {
-        for (int i = 0; i < n; i++) {
-            if (arr[i][j] != 0) {
-                arr[i][j] = -1;
-            }
-        }
-    }
-
-    public static void markMatrix(int[][] arr, int n, int m) {
-        for (int i = 0; i < n; i++) {
+    private static void optimalFillFirstRowColWithZero(int[][] arr, int n, int m, int colZero) {
+        if (arr[0][0] == 0) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
-                    markRows(m, arr, i);
-                    markColums(n, arr, j);
+                arr[0][j] = 0;
+            }
+        }
+        if (colZero == 0) {
+            for (int i = 0; i < n; i++) {
+                arr[i][0] = 0;
+            }
+        }
+    }
+
+    private static void optimalFillFromOneWithZero(int[][] arr, int n, int m) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (arr[i][j] != 0) {
+                    if (arr[i][0] == 10 || arr[0][j] == 0) {
+                        arr[i][j] = 0;
+                    }
                 }
             }
         }
     }
 
-    public static int[][] setMatrixToZerosBRute(int[][] arr, int m, int n) {
-        markMatrix(arr, n, m);
+    private static void optimalFillArrWithZero(int[][] arr, int n, int m, int colZero) {
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] == -1) {
+                if (arr[i][j] == 0) {
+                    arr[i][0] = 0;
+                    if (j != 0) {
+                        arr[0][j] = 0;
+                    } else {
+                        colZero = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void betterSolution(int[][] arr, int n, int m) {
+        int[] row = new int[n];
+        int[] col = new int[m];
+        fillRowCol(arr, n, m, row, col);
+        fillArrWithZeros(arr, n, m, row, col);
+        printAns(arr);
+    }
+
+    private static void fillArrWithZeros(int[][] arr, int n, int m, int[] row, int[] col) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
                     arr[i][j] = 0;
                 }
             }
         }
-        return arr;
     }
 
-    public static int[][] setMatrixToZerosBetter(int n, int m, int[][] arr) {
-        int[] row = new int[n];
-        int[] col = new int[m];
-        Arrays.fill(row, 0);
-        Arrays.fill(col, 0);
+    private static void fillRowCol(int[][] arr, int n, int m, int[] row, int[] col) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 0) {
@@ -67,48 +95,48 @@ public class SetMatrixZero {
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (row[i] == 1 || col[j] == 1) {
-                    arr[i][j] = 0;
-                }
-            }
-        }
-        return arr;
     }
 
-    public static int[][] setMatrixToZerosOptimal(int[][] arr, int n, int m) {
-        int col0 = -1;
+    private static void bruteSolution(int[][] arr, int n, int m) {
+        markMinusOne(arr, n, m);
+        markZero(arr, n, m);
+        printAns(arr);
+    }
+
+    private static void printAns(int[][] arr) {
+        for (var el : arr) {
+            System.out.println(Arrays.toString(el));
+        }
+    }
+
+    private static void markZero(int[][] arr, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == -1) arr[i][j] = 0;
+            }
+        }
+    }
+
+    private static void markMinusOne(int[][] arr, int n, int m) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 0) {
-                    if (i != 0)
-                        arr[i][0] = 0;
-                    else col0 = 0;
-
-                    arr[0][j] = 0;
+                    markRow(arr, n, j);
+                    markCol(arr, m, i);
                 }
             }
         }
-        for(int i =1; i < n; i++){
-            for(int j =1; j < m; j++){
-                if(arr[i][0]==0 || arr[0][j]==0){
-                    arr[i][j]=0;
-                }
-            }
-        }
-        if(arr[0][0]==0){
-            for(int j =0; j<m; j++){
-                arr[0][j]=0;
-            }
-        }
-        if(col0==0){
-            for(int i =0; i < n; i++){
-                arr[i][0]=0;
-            }
-
-        }
-        return arr;
     }
 
+    private static void markCol(int[][] arr, int m, int i) {
+        for (int col = 0; col < m; col++) {
+            if (arr[i][col] != 0) arr[i][col] = -1;
+        }
+    }
+
+    private static void markRow(int[][] arr, int n, int j) {
+        for (int row = 0; row < n; row++) {
+            if (arr[row][j] != 0) arr[row][j] = -1;
+        }
+    }
 }
