e82fa7d6734c5694df66c6b8c27de706d795dded
Marcin <gasiorek.mail@gmail.com>
2023-07-25 20:06:39 +0200

    Dijkstra Algorithm implementation - find the shortest path made use of record Pair instead of 3x List

--- src/dijkstraalgo/DijkstraAlgoImpl.java
+++ src/dijkstraalgo/DijkstraAlgoImpl.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
-record Pair(int distance, int node) {
+
+record Pair(int node, int distance) {
 }
 
 public class DijkstraAlgoImpl {
@@ -16,16 +17,16 @@ public class DijkstraAlgoImpl {
         int nodesNum = 6;
         int edgesNum = 8;
         int sourceNode = 0;
+        List<Pair> node0 = new ArrayList<>(List.of(new Pair(1, 4), new Pair(2, 4)));
+        List<Pair> node1 = new ArrayList<>(List.of(new Pair(0, 4), new Pair(2, 2)));
+        List<Pair> node2 = new ArrayList<>(List.of(new Pair(0, 4), new Pair(1, 2),
+                new Pair(3, 3), new Pair(4, 1), new Pair(5, 6)));
+        List<Pair> node3 = new ArrayList<>(List.of(new Pair(2, 3), new Pair(5, 2)));
+        List<Pair> node4 = new ArrayList<>(List.of(new Pair(2, 1), new Pair(5, 3)));
+        List<Pair> node5 = new ArrayList<>(List.of(new Pair(3, 2), new Pair(2, 6),
+                new Pair(4, 3)));
 
-        List<List<Integer>> node0 = new ArrayList<>(List.of(List.of(1, 4), List.of(2, 4)));
-        List<List<Integer>> node1 = new ArrayList<>(List.of(List.of(0, 4), List.of(2, 2)));
-        List<List<Integer>> node2 = new ArrayList<>(List.of(List.of(0, 4), List.of(1, 2),
-                List.of(3, 3), List.of(4, 1), List.of(5, 6)));
-        List<List<Integer>> node3 = new ArrayList<>(List.of(List.of(2, 3), List.of(5, 2)));
-        List<List<Integer>> node4 = new ArrayList<>(List.of(List.of(2, 1), List.of(5, 3)));
-        List<List<Integer>> node5 = new ArrayList<>(List.of(List.of(3, 2), List.of(2, 6), List.of(4, 3)));
-
-        List<List<List<Integer>>> adjecencyList = new ArrayList<>();
+        List<List<Pair>> adjecencyList = new ArrayList<>();
         adjecencyList.add(node0);
         adjecencyList.add(node1);
         adjecencyList.add(node2);
@@ -36,31 +37,32 @@ public class DijkstraAlgoImpl {
         System.out.println(Arrays.toString(dijkstraFindShortestWay(nodesNum, sourceNode, adjecencyList)));
     }
 
-    private static int[] dijkstraFindShortestWay(int nodesNum, int sourceNode, List<List<List<Integer>>> adjecencyList) {
+    private static int[] dijkstraFindShortestWay(int nodesNum, int sourceNode, List<List<Pair>> adjecencyList) {
         PriorityQueue<Pair> pq = new PriorityQueue<>((x, y) -> x.distance() - y.distance());
-        int[] dist = new int[nodesNum];
-        for (int i = 0; i < dist.length; i++) {
-            dist[i] = Integer.MAX_VALUE;
+        int[] shortestDistanceAns = new int[nodesNum];
+        for (int i = 0; i < shortestDistanceAns.length; i++) {
+            shortestDistanceAns[i] = Integer.MAX_VALUE;
         }
-        System.out.println(Arrays.toString(dist));
-        dist[sourceNode] = 0;
-        pq.add(new Pair(0, sourceNode));
+
+        shortestDistanceAns[sourceNode] = 0;
+        pq.add(new Pair(sourceNode, 0));
 
         while (pq.size() != 0) {
+
             int distance = pq.peek().distance();
             int node = pq.peek().node();
             pq.remove();
 
             for (int i = 0; i < adjecencyList.get(node).size(); i++) {
-                int edgeWeight = adjecencyList.get(node).get(i).get(1);
-                int adjNode = adjecencyList.get(node).get(i).get(0);
+                int simpleEdgeDist = adjecencyList.get(node).get(i).distance();
+                int adjNode = adjecencyList.get(node).get(i).node();
 
-                if (distance + edgeWeight < dist[adjNode]) {
-                    dist[adjNode] = distance + edgeWeight;
-                    pq.add(new Pair(dist[adjNode], adjNode));
+                if (distance + simpleEdgeDist < shortestDistanceAns[adjNode]) {
+                    shortestDistanceAns[adjNode] = distance + simpleEdgeDist;
+                    pq.add(new Pair(adjNode, shortestDistanceAns[adjNode]));
                 }
             }
         }
-        return dist;
+        return shortestDistanceAns;
     }
 }
